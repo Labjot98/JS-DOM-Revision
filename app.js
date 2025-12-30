5b3cf003054ef14628226365a392c89652ff37ea
Labjot Singh <labjotsingh2004@gmail.com>
2025-12-30 23:57:51 +0000

    Tring DOM Manipulation

---
+++ app.js
@@ -0,0 +1,8 @@
+let heading = document.body.querySelector("h1");
+console.log(heading.innerText);
+
+heading.innerText = "Peter Parker";
+console.log(heading);
+
+heading.innerText = "Iron Man"
+console.log(heading);
